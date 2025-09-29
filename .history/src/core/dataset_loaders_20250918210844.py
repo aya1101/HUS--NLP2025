a44a3ceb467a33f076d13a8771d611a02fd77a6c
Your Name <you@example.com>
2025-09-30 00:50:10 +0700

    week 2

--- .history/src/core/dataset_loaders_20250918210844.py
+++
@@ -1,4 +0,0 @@
-def load_raw_text_data(file_path: str) -> str:
-    """Load raw text data from a given file path."""
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
--- .history/src/preprocessing/simple_tokenizer_20250918122743.py
+++
@@ -1,4 +0,0 @@
-from src.core.interfaces import Tokenizer
-class SimpleTokenizer(Tokenizer):
-    def tokenize(self, text: str) -> list[str]:
-        return text.lower().split()
